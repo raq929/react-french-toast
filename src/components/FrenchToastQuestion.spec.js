e54c7a2d3e0b454fa9bc8c56e0b8942e4d120580
racheltstevens <racheltstevens@gmail.com>
2016-04-05 17:14:45 -0400

    Sets up spec file

---
+++ src/components/FrenchToastQuestion.spec.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+import cheerio from 'cheerio';
+import AboutPage from './AboutPage';
+import React from 'react';
+import ReactDOMServer from 'react/lib/ReactDOMServer';
+
+chai.should();
+
