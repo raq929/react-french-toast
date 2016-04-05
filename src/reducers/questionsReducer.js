934880e6b472b5447459cd7c60d0ace9c6a1c489
racheltstevens <racheltstevens@gmail.com>
2016-04-05 14:57:12 -0400

    questionsReducer correctly sets state after createItem is called

--- src/reducers/questionsReducer.js
+++ src/reducers/questionsReducer.js
@@ -1,5 +1,5 @@
 import objectAssign from 'object-assign';
-import {CHOOSE_ITEM} from '../constants/ActionTypes';
+import {CHOOSE_ITEM, CREATE_ITEM} from '../constants/ActionTypes';
 
 const initialState = {
   items: [
@@ -26,31 +26,38 @@ const initialState = {
 
 export default function frenchToastAppState(state = initialState, action){
   
-  switch(action.type) {
-    case CHOOSE_ITEM: 
-    { 
-      //if there is another question, ask it, else ask if it is the thing. 
-      let newState = objectAssign({}, state);
-      //add this question to the asked array
-      //TODO: add other question to asked array?
-      newState.asked.push(action.choice);
-      //change this item to choiceLeft
-      newState.choiceLeft = action.choice;
-      //if there is a next, and it hasn't been asked, make that choiceRight
-      if (newState.items[action.choice].next &&
-       newState.asked.indexOf(newState.items[action.choice].next === -1)){
+  if(action.type === CHOOSE_ITEM) { 
+    //if there is another question, ask it, else ask if it is the thing. 
+    let newState = objectAssign({}, state);
+    //add this question to the asked array
+    //TODO: add other question to asked array?
+    newState.asked.push(action.choice);
+    //change this item to choiceLeft
+    newState.choiceLeft = action.choice;
+    //if there is a next, and it hasn't been asked, make that choiceRight
+    if (newState.items[action.choice].next &&
+     newState.asked.indexOf(newState.items[action.choice].next === -1)){
 
-        let nextQuestion = newState.items[action.choice].next;
-        newState.choiceRight = nextQuestion;
-      } else {
-        //if there is no next, there is no choice B
-        //TODO: change rendering for no choiceRight
-        newState.choiceRight = undefined;
-      }
-
-      return newState;
+      let nextQuestion = newState.items[action.choice].next;
+      newState.choiceRight = nextQuestion;
+    } else {
+      //if there is no next, there is no choice B
+      //TODO: change rendering for no choiceRight
+      newState.choiceRight = undefined;
     }
-    default:
-      return state;
+
+    return newState;
+  } else if (action.type === CREATE_ITEM){
+    let newState = objectAssign({}, state);
+    newState.items.push({text: action.text, next: null});
+    const lastQuestion = state.choiceLeft;
+    newState.items[lastQuestion].next = newState.items.length - 1;
+    //reset game
+    newState.choiceLeft = 1;
+    newState.choiceRight = 0;
+    newState.asked = [];
+    return newState;
+  } else {
+    return state;
   }  
 }
--- src/reducers/questionsReducer.spec.js
+++ src/reducers/questionsReducer.spec.js
@@ -1,9 +1,9 @@
 import chai, {should, expect} from 'chai';
 import frenchToastAppState from './questionsReducer';
-import {chooseItem} from "../actions/frenchToastActions";
+import {chooseItem, createItem} from "../actions/frenchToastActions";
 
 describe("questionsReducer tests", ()=>{
-  var fakeState = {
+  let fakeState = {
       items: [
         {
           text: "french toast",
@@ -50,5 +50,62 @@ describe("questionsReducer tests", ()=>{
     });
   });
 
+  
+
+  describe("if an unknown action was passed in", ()=>{
+    it("returns the state it was given", () => {
+      const unknownAction = "UNKNOWN_ACTION";
+      const notAState = "Not really a state";
+      frenchToastAppState(notAState, unknownAction).should.equal("Not really a state");
+    });
+  })
+
 
 });
+
+describe("if action CREATE_ITEM was passed in",
+ ()=>{
+
+    const fakeState = {
+      items: [
+        {
+          text: "french toast",
+          next: 3
+        },
+        {
+          text: "a greyhound",
+          next: null
+        },
+        {
+          text: "a horse",
+          next: null
+        }
+      ],
+      choiceRight: 1,
+      choiceLeft: 2,
+      asked: [0]
+
+    };
+    const fakeCreateItemAction = createItem("newItem", 1);
+    let newState = frenchToastAppState(fakeState, fakeCreateItemAction);
+    it("adds the item to the item array", () => {
+      newState.items.should.include({text: fakeCreateItemAction.text, next: null});
+    });
+    it("sets the next field to null", () => {
+      expect(newState.items[newState.items.length - 1].next).to.be.null;
+    });
+    it("does not change the other items", () => {
+      newState.items[0].text.should.equal("french toast");
+      newState.items[1].text.should.equal("a greyhound");
+
+    });
+    it("should set the next value of the previous item to it's index",()=>{
+      newState.items[fakeState.choiceLeft].next.should.equal(newState.items.length - 1);
+    });
+    it("should reset the game", ()=>{
+      newState.choiceLeft.should.equal(1);
+      newState.choiceRight.should.equal(0);
+      expect(newState.asked).to.be.empty;
+    });
+
+  });
