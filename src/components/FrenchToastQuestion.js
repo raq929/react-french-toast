857acd8908c81265f18d016224d7e11fd5172535
racheltstevens <racheltstevens@gmail.com>
2016-04-08 11:31:14 -0400

    Change let to const in FrenchToast Question
    * set up test file"

--- src/components/FrenchToastQuestion.js
+++ src/components/FrenchToastQuestion.js
@@ -2,30 +2,30 @@ import React, {PropTypes} from 'react';
 
 const frenchToastQuestion = ({chooseItem,appState}) => {
 
-  let leftChoice = appState.items[appState.choiceLeft].text;
-  let rightChoice = appState.items[appState.choiceRight].text;
+  const leftChoice = appState.items[appState.choiceLeft].text;
+  const rightChoice = appState.items[appState.choiceRight].text;
 
   const itIs = "It is " + rightChoice + "!";
   return (
       <div>
-      <p> Is it more like {leftChoice} or is it more like {rightChoice}? </p>
-      <input 
-        type="button" 
+      <p>Is it more like {leftChoice} or is it more like {rightChoice}?</p>
+      <input
+        type="button"
         value={leftChoice}
         onClick={() => chooseItem(appState.choiceLeft)}
       />
-      <input 
-        type="button" 
+      <input
+        type="button"
         value={rightChoice}
         onClick={() => chooseItem(appState.choiceRight)}
       />
       <br/>
-      <input 
-        type="button" 
+      <input
+        type="button"
         value={itIs}
       />
       </div>
-    ); 
+    );
 };
 
   export default frenchToastQuestion;
--- src/components/FrenchToastQuestion.spec.js
+++ src/components/FrenchToastQuestion.spec.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import cheerio from 'cheerio';
-import AboutPage from './AboutPage';
+import FrenchToastQuestion from './FrenchToastQuestion';
 import React from 'react';
 import ReactDOMServer from 'react/lib/ReactDOMServer';
 
