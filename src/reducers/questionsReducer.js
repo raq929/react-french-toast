22901d60d2f58a38cc8cbe40339a30a969205f3e
racheltstevens <racheltstevens@gmail.com>
2016-04-08 17:44:05 -0400

    Adds tests and functionality to questions reducer"

--- src/reducers/questionsReducer.js
+++ src/reducers/questionsReducer.js
@@ -2,54 +2,66 @@ import objectAssign from 'object-assign';
 import {CHOOSE_ITEM, CREATE_ITEM} from '../constants/ActionTypes';
 
 
-const itemsArray = JSON.parse(window.localStorage.getItem('items')) || [
-    {
-      text: "french toast",
-      next: 3
-    },
-    {
-      text: "a car",
-      next: 2
-    },
-    { text: "a greyhound",
-      next: null
-    },
-    {
-      text: "a rock",
-      next: null
-    }
-  ];
+export default function frenchToastAppState(state, action){
+  if(!state){
+    const itemsArray = function(){
+      if(typeof window != "undefined" && window.localStorage.getItem('items')){
+        return JSON.parse(window.localStorage.getItem('items'));
+      }
+      return [
+        {
+          text: "french toast",
+          next: 3
+        },
+        {
+          text: "a car",
+          next: 2
+        },
+        { text: "a greyhound",
+          next: null
+        },
+        {
+          text: "a rock",
+          next: null
+        }
+      ];
+    };
 
-
-const initialState = {
-  items: itemsArray,
-  choiceLeft: 0,
-  choiceRight: 1,
-  asked: []
-};
-
-export default function frenchToastAppState(state = initialState, action){
+    state = {
+      items: itemsArray(),
+      choiceLeft: 0,
+      choiceRight: 1,
+      asked: [0, 1]
+    };
+  }
 
   if(action.type === CHOOSE_ITEM) {
     //if there is another question, ask it, else ask if it is the thing.
     let newState = objectAssign({}, state);
     //add this question to the asked array
-    //TODO: add other question to asked array?
-    newState.asked.push(action.choice);
+    if(newState.asked.indexOf(action.choice)=== -1){
+      newState.asked.push(action.choice);
+    }
     //change this item to choiceLeft
     newState.choiceLeft = action.choice;
     //if there is a next, and it hasn't been asked, make that choiceRight
-    if (newState.items[action.choice].next &&
-     newState.asked.indexOf(newState.items[action.choice].next === -1)){
-
-      let nextQuestion = newState.items[action.choice].next;
+    const nextQuestion = newState.items[action.choice].next;
+    if (nextQuestion &&
+      newState.asked.indexOf(nextQuestion) === -1){
       newState.choiceRight = nextQuestion;
     } else {
-      //if there is no next, there is no choice B
-      //TODO: change rendering for no choiceRight
-      newState.choiceRight = undefined;
+      for(let i = 0 ; i < newState.items.length; i++){
+        if (newState.asked.indexOf(i) === -1){
+          newState.choiceRight = i;
+          break;
+        } else {
+          newState.choiceRight = undefined;
+        }
+      }
     }
-
+    if(newState.choiceRight){
+      newState.asked.push(newState.choiceRight);
+    }
     return newState;
   } else if (action.type === CREATE_ITEM){
       let newState = objectAssign({}, state);
@@ -57,12 +69,14 @@ export default function frenchToastAppState(state = initialState, action){
       const lastQuestion = state.choiceLeft;
       newState.items[lastQuestion].next = newState.items.length - 1;
       //reset game
-      newState.choiceLeft = initialState.choiceLeft;
-      newState.choiceRight = initialState.choiceRight;
-      newState.asked = initialState.asked;
+      newState.choiceLeft = 0;
+      newState.choiceRight = 1;
+      newState.asked = [];
       //pass items list to localStorage
-      let localStorage = window.localStorage;
-      localStorage.setItem('items', JSON.stringify(newState.items));
+      if(typeof(Storage) !== "undefined") {
+        let localStorage = window.localStorage;
+        localStorage.setItem('items', JSON.stringify(newState.items));
+      }
       return newState;
   } else {
     return state;
--- src/reducers/questionsReducer.spec.js
+++ src/reducers/questionsReducer.spec.js
@@ -19,20 +19,32 @@ describe("questionsReducer tests", ()=>{
         {
           text: "a rock",
           next: null
+        },
+        {
+          text: "a ring",
+          next: null
         }
       ],
       choiceLeft: 1,
       choiceRight: 0,
-      asked: []
+      asked: [0]
     };
 
+  // describe("if no state was passed in", ()=>{
+  //   it("should return the default state", () => {
+  //     const fakeAction = chooseItem(0);
+  //     const newState = frenchToastAppState(null, fakeAction);
+  //     newState.choiceRight.should.equal(0);
+  //     newState.choiceLeft.should.equal(1);
+  //     expect(newState.asked).to.be.empty;
+  //   });
+  // });
 
   describe("if right choice (0) was chosen", ()=>{
     const fakeAction = chooseItem(0);
     const newState = frenchToastAppState(fakeState, fakeAction);
 
     it('it sets left choice to the chosen item', ()=>{
-      
       newState.choiceLeft.should.equal(0);
     });
     it('sets the right choice to the next item', ()=>{
@@ -42,15 +54,34 @@ describe("questionsReducer tests", ()=>{
       newState.asked.should.include(0);
     });
   });
+
   describe("if an item with no next field was chosen", ()=>{
-    const fakeAction = chooseItem(3);
-    const newState = frenchToastAppState(fakeState, fakeAction);
-    it("should set choiceRight to undefined",()=> {
+    it("should set choiceRight to the next unasked question",()=> {
+      const fakeAction = chooseItem(2);
+      fakeState.asked = [0,1,3];
+      const newState = frenchToastAppState(fakeState, fakeAction);
+      expect(newState.choiceRight).to.equal(4);
+    });
+  });
+
+  describe("if there are no unasked questions", ()=>{
+    it("should set choiceRight to undefined", ()=>{
+      const fakeAction = chooseItem(4);
+      fakeState.asked = [0,1,2,3];
+      const newState = frenchToastAppState(fakeState, fakeAction);
       expect(newState.choiceRight).to.be.undefined;
     });
   });
 
-  
+  describe("if next has already been asked", ()=>{
+    it("should set choiceRight to the next unasked question",()=> {
+      const fakeAction = chooseItem(0);
+      fakeState.asked = [0,1,3];
+      const newState = frenchToastAppState(fakeState, fakeAction);
+      console.log(newState);
+      expect(newState.choiceRight).to.equal(2);
+    });
+  });
 
   describe("if an unknown action was passed in", ()=>{
     it("returns the state it was given", () => {
@@ -58,7 +89,7 @@ describe("questionsReducer tests", ()=>{
       const notAState = "Not really a state";
       frenchToastAppState(notAState, unknownAction).should.equal("Not really a state");
     });
-  })
+  });
 
 
 });
@@ -103,8 +134,8 @@ describe("if action CREATE_ITEM was passed in",
       newState.items[fakeState.choiceLeft].next.should.equal(newState.items.length - 1);
     });
     it("should reset the game", ()=>{
-      newState.choiceLeft.should.equal(1);
-      newState.choiceRight.should.equal(0);
+      newState.choiceLeft.should.equal(0);
+      newState.choiceRight.should.equal(1);
       expect(newState.asked).to.be.empty;
     });
 
