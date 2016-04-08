f35f86d981075e4251ce0068e647b70a5674f215
racheltstevens <racheltstevens@gmail.com>
2016-04-08 11:35:10 -0400

    Set up test file for FrenchToastPage

---
+++ src/containers/FrenchToastPage.spec.js
@@ -0,0 +1,7 @@
+import chai from 'chai';
+import cheerio from 'cheerio';
+import FrenchToastPage from './FrenchToastPage';
+import React from 'react';
+import ReactDOMServer from 'react/lib/ReactDOMServer';
+
+chai.should();
